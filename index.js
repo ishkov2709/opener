999cc4fbc304e673bff4f931a9b5a8bb77cbf329
ishkov2709 <dmitryishkov27@gmail.com>
2024-04-22 22:22:51 +0300

    add close page to onload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload = function () {
-  window.location.replace("https://www.youtube.com/");
+  window.open("https://www.youtube.com/", "_blank");
   window.open("https://www.facebook.com/", "_blank");
   window.open("https://www.google.com/", "_blank");
-};
 
-window.close();
+  window.close();
+};
