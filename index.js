46b64fcf7fa4a400b615ea26c5d559e8199e1322
ishkov2709 <dmitryishkov27@gmail.com>
2024-04-22 22:10:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  window.location.replace("https://www.youtube.com/");
+  window.open("https://www.facebook.com/", "_blank");
+  window.open("https://www.google.com/", "_blank");
+};
+
+window.close();
